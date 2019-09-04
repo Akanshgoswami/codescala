17e2a03aa72e8aad48f15cccf0e912173686f057
akansh <goswamiakansh@gmail.com>
2019-09-05 00:26:24 +0530

    new classes added

--- src/main/scala/com/app/dbda/BusinessComputation.scala
+++ src/main/scala/com/app/dbda/BusinessComputation.scala
@@ -2,19 +2,24 @@ package com.app.dbda
 
 object BusinessComputation extends App with Connection {
 
-  val data = spark.read.format("csv").options(Map("header"->"true","inferschema"-> "true")).load("file:///home/hduser/Documents/ds/SFFoodProgram/businesses_plus.csv")
-
-  data.show(10,false)
+  val data = spark.read.format("csv")
+             .options(Map("header"->"true","inferschema"-> "true"))
+             .load("file:///home/hduser/Documents/ds/SFFoodProgram/businesses_plus.csv")
+//data.printSchema()
+//  data.show(10,false)
 
   import spark.implicits._
   import org.apache.spark.sql.functions.struct
   import org.apache.spark.sql.functions.{concat, lit,udf}
   import org.apache.spark.sql.functions._
 
+var df = data.select(data.col("city") as ("bcity"),$"owner_city",$"business_id")
+//  df.show(5)
 
+//df.piv
 
-
-
+var pvdf=  df.groupBy("bcity").pivot("owner_city").sum("business_id")
+ pvdf.show(5)
 
 
 
---
+++ src/main/scala/com/app/method/MethodExample.scala
@@ -0,0 +1,20 @@
+package com.app.method
+
+import scala.io.Source._
+
+object MethodExample extends App {
+/*//  Take input from console..
+ println( scala.io.Source.stdin.getLines().take(2).map(_.toInt).sum)
+
+  println("enter 5 number")
+
+ println( scala.io.Source.stdin.getLines().take(5).map(_.toInt).sum)*/
+
+
+
+  def f(n:Int) :Unit ={println("Hello world"+n.toString); if(n>1)f(n-1)}
+  f(3)
+
+  def d(n:Int) :Unit ={  if(n>1)d(n-1); println(n+"Hello world");}
+  d(3)
+}
